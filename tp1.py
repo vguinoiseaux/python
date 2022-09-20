a5fe9c82c35699ece7f9fdd07fa7d81fc0f370dd
vguinoiseaux <112979643+vguinoiseaux@users.noreply.github.com>
2022-09-20 08:52:25 +0200

    Add files via upload

---
+++ tp1.py
@@ -0,0 +1,61 @@
+"""
+a=4
+carre=a**2
+print("la valeur de a",a,"au carré est",carre,"")
+"""
+"""
+kilometre=100
+miles=kilometre/1.609
+print("la distance de ",kilometre,"kilomètre est de ",miles,"miles")
+"""
+"""
+a=2
+b=3
+print("avant échange a=",a,"et b=",b)
+a=b
+b=a
+print("après échange a=",a,"et b=",b)
+"""
+"""
+valeur_tva=20
+
+prix_ht=40
+tva=prix_ht*valeur_tva/100;
+prix_ttc=prix_ht+tva;
+
+print("Prix HT du produit: ",prix_ht," €")
+print ("Indice de la TVA en 2015: ",valeur_tva," %")
+print ("Valeur de la TVA: ",tva," €")
+print ("Prix TTC: ",prix_ttc," €")
+"""
+"""
+a=3
+b=-15
+if a<=0:
+    print("a est inférieur")
+else:
+    print("a est superieur")
+if b<=0:
+    print("b est inférieur")
+else:
+    print("b est superieur")
+"""
+"""
+age=8
+if age>=6 and age<=7:
+    print("Poussin")
+elif age>=8 and age<=9:
+    print("Pupille")
+"""
+"""
+nb_valeur=5
+result=0
+for i in range(0,nb_valeur+1):
+    result=result+i
+print("Somme des ",nb_valeur," premiers nombres entiers est: ",result)
+"""
+"""
+n=int(input("choisi un chiffre "))
+for i in range (1,11):
+    print(n,"*",i,"=",n*i)
+"""
